2e7bc5fab6c6267fc0fa8a99a0be9cf9e9cfcfe2
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-03-22 12:22:26 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasljedivanje2
+{
+    class GeometrijskiLik
+    {
+        protected int brojStranica;
+        protected double[] stranice= new double[10];
+
+        public void UcitajStranice()
+        {
+            for(int i =0 ;i<brojStranica; i++)
+            {
+                Console.WriteLine("Upiši duljinu {0}. stranice ", i+1);
+                string s = Console.ReadLine();
+                stranice[i] = Convert.ToDouble(s);
+            }
+        }
+
+        public double Opseg()
+        {
+            double opseg = 0;
+            for (int i = 0; i < brojStranica; i++) 
+            { 
+                opseg += stranice[i];
+            }
+            return opseg;
+        }
+    }
+
+    class Trokut : GeometrijskiLik
+    {
+        public Trokut()
+        {
+            brojStranica = 3;
+        }
+    
+    }
+    
+    class Cetverokut : GeometrijskiLik
+        {
+            public Cetverokut()
+            {
+                brojStranica = 4;
+            }
+        }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trokut t = new Trokut();
+            t.UcitajStranice();
+            Console.WriteLine("Opseg trkokuta je: {0} ", t.Opseg());
+
+            Cetverokut c = new Cetverokut();
+            c.UcitajStranice();
+            Console.WriteLine("Opseg četverokuta je: {0} ", c.Opseg());
+
+
+            Console.ReadKey();
+
+           
+        }
+    }
+}
